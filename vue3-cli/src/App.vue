70445571662a46b7ea95821b1eb9eb3c0db8c62a
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2024-06-20 14:23:48 +0900

    vue CLI로 프로젝트 생성

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <AppHeader 
+    v-bind:appTitle="message"
+    v-on:change="changeMessage">
+  </AppHeader>
+</template>
+
+<script>
+// import 컴포넌트이름 from './컴포넌트경로'
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  components: {
+    // '컴포넌트이름': 컴포넌트내용
+    // 'AppHeader': AppHeader
+    AppHeader
+  },
+  data() {
+    return {
+      message: 'AppHeader 컴포넌트'
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.message = '변경됨'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
